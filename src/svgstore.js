34be8f98dc616c1b0f2755ea0b0b785950582d01
ScriptedAlchemy <zjackson@lululemon.com>
2019-07-01 13:32:59 -0700

    fix: updating compile

--- src/svgstore.js
+++ src/svgstore.js
@@ -18,27 +18,7 @@ const path = require('path');
 const utils = require('./helpers/utils');
 const ConstDependency = require('webpack/lib/dependencies/ConstDependency');
 const NullFactory = require('webpack/lib/NullFactory');
-
-function compatAddPlugin(tappable, hookName, callback, async = false, forType = null) {
-  const method = (async) ? 'tapPromise' : 'tap';
-  if (tappable.hooks) {
-    if (forType) {
-      tappable.hooks[hookName][method](forType, WebpackSvgStore.name, callback);
-    } else {
-      tappable.hooks[hookName][method](WebpackSvgStore.name, callback);
-    }
-  } else {
-    tappable.plugin(hookName, callback);
-  }
-}
-
-const allowedMagicVariables = [
-  '__svg__',
-  '__sprite__',
-  '__svgstore__',
-  '__svgsprite__',
-  '__webpack_svgstore__'
-];
+const async = require('async');
 
 class WebpackSvgStore {
 
@@ -60,7 +40,6 @@ class WebpackSvgStore {
       this.tasks[file].push(value);
     })();
   }
-
 
   createTaskContext(expr, parser) {
     const data = {
@@ -97,46 +76,106 @@ class WebpackSvgStore {
 
   apply(compiler) {
     // AST parser
-    compatAddPlugin(compiler, 'compilation', (compilation, data) => {
-      compilation.dependencyFactories.set(ConstDependency, new NullFactory());
-      compilation.dependencyTemplates.set(ConstDependency, new ConstDependency.Template());
-      compatAddPlugin(data.normalModuleFactory, 'parser', (parser) => {
-        compatAddPlugin(parser, 'statement', (expr) => {
-          if (!expr.declarations || !expr.declarations.length) return;
-          const thisExpr = expr.declarations[0];
-          if (allowedMagicVariables.indexOf(thisExpr.id.name) > -1) {
-            return this.createTaskContext(thisExpr, parser);
-          }
-        });
-      }, false, 'javascript/auto');
-
-      // save file to fs
-      compatAddPlugin(compilation, 'additionalAssets', () => {
-        const taskKeysArr = Object.keys(this.tasks);
-        if (taskKeysArr.length === 0) return Promise.resolve();
-        else {
-          return Promise.all(taskKeysArr.map(async (key) => {
-            const tasksJobs = this.tasks[key];
-            return Promise.all(tasksJobs.map(async task => {
-              // add sprite to assets
-              compilation.assets[task.fileName] = {
-                size: () => Buffer.byteLength(task.fileContent, 'utf8'),
-                source: () => Buffer.from(task.fileContent)
-              };
-            }))
-          }));
-        }
-      }, true);
-
-      compatAddPlugin(compilation, 'afterSeal', () => {
-        this.tasks = {};
+    if (compiler.hooks) { //webpack ^4.0.0
+      compiler.hooks.compilation.tap({name: 'WebpackSvgStore'}, (compilation) => {
+        compilation.dependencyFactories.set(ConstDependency, new NullFactory());
+        compilation.dependencyTemplates.set(ConstDependency, new ConstDependency.Template());
       });
 
-    });
+      compiler.hooks.normalModuleFactory.tap({name: 'WebpackSvgStore'}, factory => {
+        factory.hooks.parser.for('javascript/auto').tap({name: 'WebpackSvgStore'}, (parser, options) => {
+          parser.hooks.statement.tap({name: 'WebpackSvgStore'}, (expr) => {
+            if (!expr.declarations || !expr.declarations.length) return;
+            const thisExpr = expr.declarations[0];
+            if ([
+              '__svg__',
+              '__sprite__',
+              '__svgstore__',
+              '__svgsprite__',
+              '__webpack_svgstore__'
+            ].indexOf(thisExpr.id.name) !== -1) {
+              return this.createTaskContext(thisExpr, parser);
+            }
+          });
+        });
+      });
 
+      // save file to fs
+      compiler.hooks.emit.tap({name: 'WebpackSvgStore'}, (compilation, callback) => {
+        async.forEach(Object.keys(this.tasks),
+          (key, outerCallback) => {
+            async.forEach(this.tasks[key],
+              (task, callback) => {
+                // add sprite to assets;
+                compilation.assets[task.fileName] = {
+                  size: function () {
+                    return Buffer.byteLength(task.fileContent, 'utf8');
+                  },
+                  source: function () {
+                    return new Buffer(task.fileContent);
+                  }
+                };
+                // done
+                callback();
+              }, outerCallback);
+          }, callback);
+      });
+
+      compiler.hooks.done.tap({name: 'WebpackSvgStore'}, () => {
+        this.tasks = {};
+      });
+    } else {
+      // AST parser
+      compiler.plugin('compilation', (compilation, data) => {
+
+        compilation.dependencyFactories.set(ConstDependency, new NullFactory());
+        compilation.dependencyTemplates.set(ConstDependency, new ConstDependency.Template());
+
+        data.normalModuleFactory.plugin('parser', (parser, options) => {
+          parser.plugin('statement', (expr) => {
+            if (!expr.declarations || !expr.declarations.length) return;
+            const thisExpr = expr.declarations[0];
+            if ([
+              '__svg__',
+              '__sprite__',
+              '__svgstore__',
+              '__svgsprite__',
+              '__webpack_svgstore__'
+            ].indexOf(thisExpr.id.name) > -1) {
+              return this.createTaskContext(thisExpr, parser);
+            }
+          });
+        });
+      });
+
+      // save file to fs
+      compiler.plugin('emit', (compilation, callback) => {
+        async.forEach(Object.keys(this.tasks),
+          (key, outerCallback) => {
+            async.forEach(this.tasks[key],
+              (task, callback) => {
+                // add sprite to assets
+                compilation.assets[task.fileName] = {
+                  size: function () {
+                    return Buffer.byteLength(task.fileContent, 'utf8');
+                  },
+                  source: function () {
+                    return new Buffer(task.fileContent);
+                  }
+                };
+                // done
+                callback();
+              }, outerCallback);
+          }, callback);
+      });
+
+      compiler.plugin('done', () => {
+        this.tasks = {};
+      });
+    }
   }
-
 }
+
 
 /**
  * Return function
